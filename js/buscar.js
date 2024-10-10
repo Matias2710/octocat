1b7dc1c1cbe6c37f74d597f77fc2853a6b21b9a4
Matias2710 <155513340+Matias2710@users.noreply.github.com>
2024-10-10 11:38:20 -0600

    Update buscar.js

--- js/buscar.js
+++ js/buscar.js
@@ -1,8 +1,8 @@
 function toggleSearch() {
     var searchBar = document.getElementById("search-bar");
     if (searchBar.style.display === "none") {
-        searchBar.style.display = "flex";  // Mostrar la barra de búsqueda
+        searchBar.style.display = "flex";
     } else {
-        searchBar.style.display = "none";   // Ocultar la barra de búsqueda
+        searchBar.style.display = "none";
     }
 }
