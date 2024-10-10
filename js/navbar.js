607c0ba84624501a3d0d6916dd51d3d7987f0b0c
Matias2710 <155513340+Matias2710@users.noreply.github.com>
2024-10-10 11:38:35 -0600

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -1,22 +1,13 @@
 function menubar() {
-    var hihu = document.querySelector('.menu-data');
-    var element = document.getElementById('Menu-bar');
-
-    if (hihu) {
-        hihu.classList.remove('animate__fadeOut');
-        element.style.display = 'none';
-        hihu.style.zIndex = '1';
-        hihu.classList.add('animate__fadeIn');
+    var menu = document.querySelector('.menu-data');
+    if (menu) {
+        menu.classList.add('show');
     }
 }
 
 function closebar() {
-    var hihu = document.querySelector('.menu-data');
-    var element = document.getElementById('Menu-bar');
-    if (hihu) {
-        hihu.classList.remove('animate__fadeIn');
-        hihu.style.zIndex = '-20';
-        element.style.display = 'block';
-        hihu.classList.add('animate__fadeOut');
+    var menu = document.querySelector('.menu-data');
+    if (menu) {
+        menu.classList.remove('show');
     }
 }
